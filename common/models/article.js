5e31956b4b8352654375d353f3bb3d79a3f461b5
Wayne <evilcome.com@gmail.com>
2017-05-09 13:09:32 +0800

    add beforeRemote hook

--- common/models/article.js
+++ common/models/article.js
@@ -7,9 +7,6 @@ module.exports = function(Article) {
     context.args.data.createdAt = Date.now();
     context.args.data.authorId = context.req.accessToken.userId;
 
-    console.log(context.req.accessToken);
-    console.log(context.args.data);
-
     next();
   });
 };
--- common/models/topic.js
+++ common/models/topic.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Topic) {
-  
+  Topic.beforeRemote('create', function(context, user, next) {
+    console.log('> Topic.beforeRemote triggered');
+
+    context.args.data.createdAt = Date.now();
+    context.args.data.authorId = context.req.accessToken.userId;
+
+    next();
+  });
 };
