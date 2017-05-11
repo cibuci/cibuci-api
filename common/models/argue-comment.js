7df9941182fbcc28cdbb6e32caf4c3e15515e733
zhangzicong <zhangzicong6@gmail.com>
2017-05-11 16:11:36 +0800

    评论

--- common/models/argue-comment.js
+++ common/models/argue-comment.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Arguecomment) {
+	var app = require('../../server/server');
 
+	Arguecomment.beforeRemote('create', function(context, comment, next) {
+	    console.log('> Arguecomment.beforeRemote triggered');
+
+	    context.args.data.createdAt = Date.now();
+	    context.args.data.authorId = context.req.accessToken.userId;
+
+	    next();
+	  });
+
+	Arguecomment.afterRemote('create',function(context, comment, next){
+		var Argue = app.models.Argue;
+		Argue.findById(comment.argueId,{},function(err, argue){
+			
+		});
+	});
 };
--- common/models/topic-comment.js
+++ common/models/topic-comment.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function(Topiccomment) {
+	var app = require('../../server/server');
 
+	Topiccomment.beforeRemote('create', function(context, comment, next) {
+	    console.log('> Topiccomment.beforeRemote triggered');
+
+	    context.args.data.createdAt = Date.now();
+	    context.args.data.authorId = context.req.accessToken.userId;
+
+	    next();
+	  });
+
+	Topiccomment.afterRemote('create',function(context, comment, next){
+		var Topic = app.models.Topic;
+		Topic.findById(comment.topicId,{},function(err, topic){
+			topic.updateAttributes({'lastReplyAt':comment.createdAt,'commentsCount':topic.commentsCount+1},function(err,instance){
+				next();
+			});
+		});
+	});
 };
