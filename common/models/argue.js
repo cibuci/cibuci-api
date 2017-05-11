d161548e6000208b6717f607a1e6c01f5f765f5f
zhangzicong <zhangzicong6@gmail.com>
2017-05-11 19:26:21 +0800

    参数必须的都加上require

--- common/models/argue.js
+++ common/models/argue.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = function(Argue) {
-  
+  Argue.beforeRemote('create', function(context, user, next) {
+    console.log('> Argue.beforeRemote triggered');
+
+    context.args.data.createdAt = Date.now();
+    context.args.data.authorId = context.req.accessToken.userId;
+
+    next();
+  });
 };
