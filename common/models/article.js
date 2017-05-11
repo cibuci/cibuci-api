97af564dd4319cba45a13c195bd93a7284d2c2d8
zhangzicong <zhangzicong6@gmail.com>
2017-05-11 20:35:24 +0800

    Readcount 增加

--- common/models/article.js
+++ common/models/article.js
@@ -1,12 +1,17 @@
 'use strict';
 
 module.exports = function(Article) {
-  Article.beforeRemote('create', function(context, user, next) {
+  Article.beforeRemote('create', function(context, artice, next) {
     console.log('> Article.beforeRemote triggered');
 
     context.args.data.createdAt = Date.now();
     context.args.data.authorId = context.req.accessToken.userId;
 
     next();
+  });
+  Article.afterRemote('findById',function(context, artice, next){
+  	artice.readCount+=1;
+  	artice.save();
+  	next();
   });
 };
